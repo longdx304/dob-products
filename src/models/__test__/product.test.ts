d1d127f69d880c0cd2b3a15e6632c78b3c584e75
Long Do <longdo@dobtech.vn>
2021-11-19 23:41:47 +0700

    add product model with optimistic concurrency control

--- src/models/__test__/product.test.ts
+++ src/models/__test__/product.test.ts
@@ -1,8 +1,50 @@
+import { Product, ProductType } from '../product';
+
 it('implements optimistic concurrency control', async () => {
   // create an instance of a product
+  const product = Product.build({
+    name: 'msi 3060ti ventus',
+    sku: 'msi-3060ti-ventus',
+    type: ProductType.Simple,
+  });
+
   // save the product to db
+  await product.save();
+
   // fetch the product twice
+  const firstInstance = await Product.findById(product.id);
+  const secondInstance = await Product.findById(product.id);
+
   // make 2 separate changes to the products we fetched
+  firstInstance!.set({ name: 'MSI 3060ti Ventus' });
+  secondInstance!.set({ name: 'MSI 3090 Ventus' });
+
   // save the first fetched product
+  await firstInstance!.save();
+
   // save the second fetched product and expect an error
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return;
+  }
+
+  throw new Error('Should not reach this point');
+});
+
+it('increments the version number on multiple saves', async () => {
+  const product = Product.build({
+    name: 'msi 3060ti ventus',
+    sku: 'msi-3060ti-ventus',
+    type: ProductType.Simple,
+  });
+
+  await product.save();
+  expect(product.version).toEqual(0);
+
+  await product.save();
+  expect(product.version).toEqual(1);
+
+  await product.save();
+  expect(product.version).toEqual(2);
 });
--- src/models/product.ts
+++ src/models/product.ts
@@ -1,5 +1,73 @@
-interface ProductAttrs {
-  model: string;
-  sku: string;
-  brand: string;
+import { Document, model, Model, Schema } from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+enum ProductType {
+  Simple = 'simple',
+  Configurable = 'configurable',
 }
+
+interface Attribute {
+  attrName: string;
+  attrValue: string[];
+  useForVariant: boolean;
+}
+
+interface ProductAttrs {
+  name: string;
+  type: ProductType;
+  sku: string;
+  attributes?: Attribute[];
+}
+
+interface ProductDoc extends Document {
+  name: string;
+  type: ProductType;
+  sku: string;
+  attributes?: Attribute[];
+  version: number;
+}
+
+interface ProductModel extends Model<ProductDoc> {
+  build(attrs: ProductAttrs): ProductDoc;
+}
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: Object.values(ProductType),
+      default: ProductType.Simple,
+    },
+    sku: {
+      type: String,
+      unique: true,
+    },
+    attributes: {
+      type: [{}],
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+productSchema.set('versionKey', 'version');
+productSchema.plugin(updateIfCurrentPlugin);
+
+productSchema.statics.build = (attrs: ProductAttrs) => {
+  return new Product(attrs);
+};
+
+const Product = model<ProductDoc, ProductModel>('Product', productSchema);
+
+export { Product, ProductType };
